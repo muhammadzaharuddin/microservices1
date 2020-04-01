a00407d90cbcef476eaad2a42964464b05ccb268
Zaharuddin <muhammad.zaharuddin@accenture.com>
2020-04-01 20:50:13 +0800

    first commit

---
+++ config/src/test/java/com/okta/developer/docker_microservices/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.developer.docker_microservices.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
